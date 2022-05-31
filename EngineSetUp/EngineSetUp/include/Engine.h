2f9e853653152df6f13094f286a94a766bcba810
Константин Костадинов <konstantinkk2000@outlook.com>
2022-05-31 16:22:05 +0300

    initial commit

---
+++ EngineSetUp/EngineSetUp/include/Engine.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cmath>
+#include <string>
+#include <float.h>
+#include <utility>
+
+#include <SDL.h>
+#include <SDL_TTF.h>
+
+#include "defines.h"
+
+SDL_Texture* LoadTexture(string configFile, SDL_Renderer* renderer);
+bool MouseIsInRect(int2 coor, SDL_Rect rect);
+bool collRectRect(SDL_Rect a, SDL_Rect b);
+bool checkInOffBounds(SDL_Rect rect, int screenWidth, int screenHeight);
+float lerp(float start, float target, float f);
---
+++ EngineSetUp/EngineSetUp/src/main.cpp
@@ -0,0 +1,25 @@
+#include "Presenter.h"
+
+#undef main
+
+int main(int* argc, char* argv[])
+{
+	Presenter presenter;
+	
+	presenter.init();
+	
+	Drawable drawable;
+
+	drawable.rect = { 0, 0, 100, 100 };
+	drawable.texture = LoadTexture("player.bmp", presenter.m_main_renderer);
+
+
+	while (1)
+	{
+		drawObject(drawable);
+		presenter.draw();
+	}
+	SDL_Delay(10000);
+	
+	return 0;
+}
